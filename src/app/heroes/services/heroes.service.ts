bdac550b8750c591343fc415924a6233634c4d59
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-24 18:29:40 +0200

    crud

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
@@ -20,33 +20,54 @@ export class HeroesService {
 
   getHeroById(id: string): Observable<Hero | undefined> {
     return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
-    .pipe(
-      catchError( error => of(undefined))
-    );
+      .pipe(
+        catchError(error => of(undefined))
+      );
+  }
+  /*
+  EXPLICACIÓN:       error => of(undefined):
+
+  Cuando ocurre un error, retorna un nuevo Observable que emite undefined en lugar de propagar el error. Esto evita que la aplicación falle debido a errores no manejados en la solicitud HTTP.
+
+
+  Capturar el error y devolver undefined como un Observable es la mejor práctica en Angular y RxJS, ya que mantiene la consistencia del tipo devuelto, facilita el manejo de datos asíncronos y permite el uso continuo de operadores de RxJS. Devolver un string undefined rompería estas ventajas y podría causar problemas en la lógica de la aplicación.
+
+  Devolver un string undefined puede parecer más simple, pero presenta varias desventajas:
+
+  Inconsistencia:
+
+  Si el método está tipado para devolver Observable<Hero | undefined>, devolver un string undefined introduce inconsistencia tipográfica.
+  Los consumidores del método podrían no estar preparados para manejar un string en lugar de un Observable.
+  Interrupción del Flujo Reactivo:
+
+  RxJS se basa en el flujo continuo de Observables. Devolver un valor fuera de este flujo puede interrumpir la cadena de operadores y complicar la lógica de manejo de datos.
+  */
+
+
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
   }
 
-/*
-EXPLICACIÓN:       error => of(undefined):
 
-Cuando ocurre un error, retorna un nuevo Observable que emite undefined en lugar de propagar el error. Esto evita que la aplicación falle debido a errores no manejados en la solicitud HTTP.
-
-
-Capturar el error y devolver undefined como un Observable es la mejor práctica en Angular y RxJS, ya que mantiene la consistencia del tipo devuelto, facilita el manejo de datos asíncronos y permite el uso continuo de operadores de RxJS. Devolver un string undefined rompería estas ventajas y podría causar problemas en la lógica de la aplicación.
-
-Devolver un string undefined puede parecer más simple, pero presenta varias desventajas:
-
-Inconsistencia:
-
-Si el método está tipado para devolver Observable<Hero | undefined>, devolver un string undefined introduce inconsistencia tipográfica.
-Los consumidores del método podrían no estar preparados para manejar un string en lugar de un Observable.
-Interrupción del Flujo Reactivo:
-
-RxJS se basa en el flujo continuo de Observables. Devolver un valor fuera de este flujo puede interrumpir la cadena de operadores y complicar la lógica de manejo de datos.
-*/
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
 
 
-getSuggestions(query:string):Observable<Hero[]>{
-  return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
-}
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+
+  deleteHeroById(hero: Hero): Observable<boolean> {
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${hero.id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      );
+  }
+
 
 }
