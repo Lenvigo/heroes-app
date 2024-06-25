ab9fdb6ae07e66465e8656f5ec72eedb3deab16d
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-25 10:56:39 +0200

    reactive forms, conexion template, getter, actualizar heroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,21 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(""),
+    superhero: new FormControl<string>("", { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(""),
+    first_appearance: new FormControl<string>(""),
+    characters: new FormControl<string>(""),
+    alt_img: new FormControl("")
+
+  });
+
 
   public publishers = [
-    {
-      id: 'DC Comics',
-      desc: 'DC - Comics '
-    },
-    {
-      id: 'Marvel Comics',
-      desc: 'Marvel - Comics '
-    },
+    { id: 'DC Comics', desc: 'DC - Comics ' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics ' },
   ];
 
+
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroById(id)
+        ),
+      ).subscribe(hero => {
+        if (!hero) { return this.router.navigateByUrl('/'); }
+        this.heroForm.reset(hero);
+        return;
+
+      })
+
+  }
+
+
+  onSubmit(): void {
+
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,  //value: this.heroForm.getRawValue(),
+    })
+
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(hero => {
+          //TODO: mostrar snackbar
+
+        });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe(hero => {
+        //TODO: mostrar snackbar y navegar a hero.id
+      });
+
+
+  }
 }
+
+
--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -8,7 +8,7 @@ export class HeroImagePipe implements PipeTransform {
 
   transform(hero: Hero): string {
     if (!hero.id && !hero.alt_img) {
-      return 'assets/no-image.img';
+      return 'assets/no-image.png';
     }
 
     if (hero.alt_img) return hero.alt_img; //va a ser un url completo
